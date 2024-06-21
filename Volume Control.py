0ff37394825849303be1f2e03727274f1c88dba4
pujan017 <119115636+pujan017@users.noreply.github.com>
2024-06-21 16:30:51 +0545

    Changes volume by the help of index and thumb fingers.

--- Volume Control.py
+++ Volume Control.py
@@ -3,7 +3,24 @@ import time
 import numpy as np
 import HandTrackingModule as htm
 import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+volRange=volume.GetVolumeRange()
+
+minVol=volRange[0]
+maxVol=volRange[1]
+vol=0
+volBar=400
+volPer=0
+area=0
 ##
 wCam, hCam=640,480
 ##
@@ -17,9 +34,36 @@ detector=htm.handDetector(detectionCon=0.7)
 
 while True:
     success, img = cap.read()
+
+    # Find Hand
+
     img=detector.findHands(img)
-    lmList,_=detector.findPosition(img,draw=False)
+    lmList,bbox=detector.findPosition(img,draw=True)
     if len(lmList) !=0:
+
+        # Filter based on size
+        area=(bbox[2]-bbox[0])*(bbox[3]-bbox[1])//100
+        # print(area)
+        if 250<area<1000:
+            # print("yes")
+
+        # Find Distance between index and Thumb
+            length,img,lineInfo=detector.findDistance(4,8,img)
+            print(length)
+
+        # Convert Volume
+
+        # Reduce Resolution to make it smoother
+
+        # Check fingers up
+
+        # If pinky is down set volume
+
+        # Drawings
+
+        # Frame rate
+
+
         # print(lmList[4],lmList[8])
 
         x1,y1=lmList[4][1],lmList[4][2]
@@ -33,8 +77,24 @@ while True:
         cv2.circle(img, (cx, cy), 15, (255, 0, 255),cv2.FILLED)
 
         length=math.hypot(x2-x1,y2-y1)
-        print(length)
+        #print(length)
 
+
+        # Hand range was 50-300
+        # Volume Range -65 - 0
+
+        vol=np.interp(length,[50,300],[minVol,maxVol])
+        volBar = np.interp(length, [50, 300], [400, 150])
+        volPer = np.interp(length, [50, 300], [0, 100])
+        # print(int(length),vol)
+        volume.SetMasterVolumeLevel(vol, None)
+
+        if length<50:
+            cv2.circle(img,(lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+
+    cv2.rectangle(img,(50,150),(85,400),(255,0,0),3)
+    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255,0,0), cv2.FILLED)
+    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), 3)
 
     cTime = time.time()
     fps=1/(cTime-pTime)
